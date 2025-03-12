1ead2e7679dce518cc6629e0827baeef8b1dbbf4
卷尺 <1352411860@qq.com>
2025-03-12 10:15:50 +0800

    feat: layout

--- src/renderer/pages/main/app.tsx
+++ src/renderer/pages/main/app.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { ResizeBox, Layout } from '@arco-design/web-react';
 
 import Header from '@/renderer/components/Header';
 import Sider from '@/renderer/components/Sider';
@@ -6,6 +7,10 @@ import StoreSider from '@/renderer/components/StoreSider';
 import StoreViewerTabs from '@/renderer/components/StoreViewerTabs';
 import { useConfigStore } from '@/renderer/store';
 import './index.css';
+
+// const Sider = Layout.Sider;
+// const Header = Layout.Header;
+// const Content = Layout.Content;
 
 const App = () => {
   const configStore = useConfigStore();
@@ -20,19 +25,49 @@ const App = () => {
   }, []);
 
   return (
-    <div className="container">
-      <Header />
-      <main className="main">
-        <Sider />
-        <div className="main-content">
-          <div style={{ width: 200 }}>
-            <StoreSider />
-          </div>
+    <Layout style={{ width: '100vw', height: '100vh' }}>
+      <Layout.Header>
+        <Header />
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider style={{ width: '53px' }}>
+          <Sider />
+        </Layout.Sider>
 
+        <Layout.Sider style={{ minWidth: 240, maxWidth: 300 }} resizeDirections={['right']}>
+          <StoreSider />
+        </Layout.Sider>
+        <Layout.Content>
           <StoreViewerTabs />
-        </div>
-      </main>
-    </div>
+        </Layout.Content>
+      </Layout>
+    </Layout>
+    // <div className="container">
+    //   <Header />
+    //   <main className="main">
+    //     <Sider />
+    //     <div className="main-content">
+    //       <ResizeBox.Split
+    //         direction="horizontal"
+    //         style={{
+    //           // height: 300,
+    //           // width: 300,
+    //           // border: '1px solid var(--color-border)',
+    //         }}
+    //         trigger={null}
+    //         size={"240px"}
+    //         max={"280px"}
+    //         min={"240px"}
+    //         panes={[<StoreSider />, <StoreViewerTabs />]}
+    //       />
+    //       {/* <div style={{ width: 200 }}>
+    //         <StoreSider />
+    //       </div>
+
+    //       <StoreViewerTabs /> */}
+    //     </div>
+    //   </main>
+    // </div>
   );
 };
 
