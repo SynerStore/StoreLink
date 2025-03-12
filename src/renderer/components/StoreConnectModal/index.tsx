847b0097e3a0bf3be5747475716234f1efd0c15f
卷尺 <1352411860@qq.com>
2025-03-12 17:33:05 +0800

    feat: 添加连接配置

--- src/renderer/components/StoreConnectModal/index.tsx
+++ src/renderer/components/StoreConnectModal/index.tsx
@@ -1,19 +1,92 @@
-import {Fragment } from "react"
-import { Modal} from "@arco-design/web-react"
+import { Fragment, useState } from 'react';
+import { Modal, Card, Space, Avatar, Typography, Grid } from '@arco-design/web-react';
 
-const StoreConnectModal =()=>{
+export type StoreConnectModalProps = {
+  children?: React.ReactNode;
+  onAddConnection?: (v: any) => void;
+};
 
-return 
-<Fragment>
-<Modal>
+const { Row, Col } = Grid;
 
+const StoreConnectModal = (props: StoreConnectModalProps) => {
+  const { children, onAddConnection } = props;
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  return (
+    <Fragment>
+      <Modal title="添加连接" visible={isModalOpen} style={{ width: 720 }} onCancel={() => setIsModalOpen(false)}>
+        <div>
+          <Row gutter={20} style={{ marginBottom: 20 }}>
+            <Col span={12}>
+              <Card hoverable>
+                <Space>
+                  <Avatar
+                    style={{
+                      backgroundColor: '#165DFF',
+                    }}
+                    size={28}
+                  >
+                    阿
+                  </Avatar>
+                  <Typography.Text>阿里云 OSS</Typography.Text>
+                </Space>
+              </Card>
+            </Col>
 
-</Modal>
-</Fragment>
+            <Col span={12}>
+              <Card hoverable>
+                <Space>
+                  <Avatar
+                    style={{
+                      backgroundColor: '#165DFF',
+                    }}
+                    size={28}
+                  >
+                    腾
+                  </Avatar>
+                  <Typography.Text>腾讯云 OSS</Typography.Text>
+                </Space>
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={20}>
+            <Col span={12}>
+              <Card hoverable>
+                <Space>
+                  <Avatar
+                    style={{
+                      backgroundColor: '#165DFF',
+                    }}
+                    size={28}
+                  >
+                    华
+                  </Avatar>
+                  <Typography.Text>华为云 OSS</Typography.Text>
+                </Space>
+              </Card>
+            </Col>
 
+            <Col span={12}>
+              <Card hoverable>
+                <Space>
+                  <Avatar
+                    style={{
+                      backgroundColor: '#165DFF',
+                    }}
+                    size={28}
+                  >
+                    ftp
+                  </Avatar>
+                  <Typography.Text>ftp</Typography.Text>
+                </Space>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </Modal>
+      <span onClick={() => setIsModalOpen(true)}>{children}</span>
+    </Fragment>
+  );
+};
 
-}
-
-
-export default StoreConnectModal
+export default StoreConnectModal;
--- src/renderer/components/StoreViewerTabs/HomeTab.tsx
+++ src/renderer/components/StoreViewerTabs/HomeTab.tsx
@@ -1,13 +1,17 @@
+import { Button } from '@arco-design/web-react';
+import StoreConnectModal from '../StoreConnectModal';
+
 const HomeTab = () => {
   return (
     <div>
-      展示所有的存储空间数量
-      展示所有的账号数量
-      展示管理按钮
-      账号添加按钮
-      展示常用的品牌添加快捷键
+      展示所有的存储空间数量 展示所有的账号数量 展示管理按钮 账号添加按钮 展示常用的品牌添加快捷键
+      <div>
+        <StoreConnectModal>
+          <Button type="primary"> 添加连接</Button>
+        </StoreConnectModal>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeTab
+export default HomeTab;
