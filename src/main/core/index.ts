3f43a848d29bf5a75023fcc8b9572d6c2dade13e
卷尺 <1352411860@qq.com>
2025-06-08 17:06:30 +0800

    chore: remove useless code dbregistry

--- src/main/core/index.ts
+++ src/main/core/index.ts
@@ -4,7 +4,6 @@ import Windows from '../windows';
 import ViewerWindowManager from '../viewer';
 import { storeRequestRegistry } from '../stores';
 import eventsRegistry from '../events/registry';
-// import { dbRegistory } from '../db';
 import { logger, isDev, installDevtool } from '../utils';
 
 export default class Core {
@@ -46,7 +45,6 @@ export default class Core {
   }
 
   private async resistry() {
-    // await dbRegistory();
     await storeRequestRegistry();
     await eventsRegistry();
   }
